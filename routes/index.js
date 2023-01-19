27e92fa3fb4e8b4e1c00e598223bf511d8062b4e
rachel howard <rdfields13@gmail.com>
2023-01-18 20:44:05 -0600

    updated routes to work with insomnia

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 const router = require("express").Router();
-const apiRoutes = require("./api");
 
-router.use("/api", apiRoutes);
+// Import all of the API routes from /api/index.js (no need for index.js though since it's implied)
+const apiUsers = require("./api/user-routes");
+const apiThoughts = require("./api/thought-routes");
+
+// add prefix of `/api` to all of the api routes imported from the `api` directory
+router.use("/api", apiUsers);
+router.use("/api", apiThoughts);
 
 router.use((req, res) => {
-  return res.send("wrong route!");
+  res.status(404).send("<h1> 404 Error!</h1>");
 });
 
 module.exports = router;
